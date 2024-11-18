d8a9c6b1df5933f04801dc730ca1c27c6d2c8815
Jluo1030 <jyluo1030@outlook.com>
2024-11-19 06:35:12 +0800

    更新 _sample_split.py 和 _winsorizer.py 文件

--- ps3/data/_sample_split.py
+++ ps3/data/_sample_split.py
@@ -1,6 +1,8 @@
 import hashlib
 
 import numpy as np
+import pandas as pd
+import random
 
 # TODO: Write a function which creates a sample split based in some id_column and training_frac.
 # Optional: If the dtype of id_column is a string, we can use hashlib to get an integer representation.
@@ -21,5 +23,13 @@ def create_sample_split(df, id_column, training_frac=0.8):
     pd.DataFrame
         Training data with sample column containing train/test split based on IDs.
     """
+    sample_size = int(len(df) * 0.8)
+    id_hash = [hashlib.md5(str(df[id_column].iloc[i]).encode("utf-8")).hexdigest() for i in range(len(df))]
+    random_id = random.sample(id_hash, sample_size)
+    for i in range(len(df)):
+        if hashlib.md5(str(df[id_column].iloc[i]).encode("utf-8")).hexdigest() in id_hash:
+            df['sample'].iloc[i] = "train"
+        else:
+            df['sample'].iloc[i] = "test"
 
     return df
